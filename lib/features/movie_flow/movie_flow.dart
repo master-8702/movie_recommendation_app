e834d6baa0745889fa6ee61efd523af3b13a77c8
Ibrahim Selman <ibroselman@gmail.com>
2023-03-09 21:50:36 +0300

    add a landing page(screen) for the app

---
+++ lib/features/movie_flow/movie_flow.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:movie_recommendation_app/features/movie_flow/landing/landing_screen.dart';
+
+class MovieFlow extends StatefulWidget {
+  const MovieFlow({Key? key}) : super(key: key);
+
+  @override
+  State<MovieFlow> createState() => _MovieFlowState();
+}
+
+class _MovieFlowState extends State<MovieFlow> {
+  // this controller is responsible for handling the navigation
+  final pageController = PageController();
+
+  void nextPage() {
+    pageController.nextPage(
+        duration: const Duration(milliseconds: 600),
+        curve: Curves.easeInOutCubic);
+  }
+
+  void previousPage() {
+    pageController.previousPage(
+        duration: const Duration(milliseconds: 600),
+        curve: Curves.easeInOutCubic);
+  }
+
+  @override
+  void dispose() {
+    //we need to dispose the controller
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: pageController,
+      // disabling the scroll behavior of the pageview because we want the users to click a button rather that just scrolling
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        LandingScreen(nextPage: nextPage, previousPage: previousPage),
+      ],
+    );
+  }
+}
